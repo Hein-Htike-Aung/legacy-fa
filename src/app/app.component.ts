37b00e0865250655691d482829e47ad67ea1dfda
Hein Htike Aung <niziu1151998@gmail.com>
2024-03-18 10:25:38 +0630

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ export interface PeriodicElement {
 const ELEMENT_DATA: PeriodicElement[] = [
   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  // { position: 4, name: 'Helium', weight: 4.0026, symbol: 'He' },
 ];
 
 @Component({
